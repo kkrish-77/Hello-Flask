38865686f0bf9714307fec7e4aa99f0e5240fa8c
kkrish-77 <kkrshsingh@gmail.com>
2025-10-31 16:47:47 +0530

    Changed configuration

--- flaskdemo.py
+++ flaskdemo.py
@@ -1,9 +1,8 @@
 #!/bin/python3
-
 from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def index():
     return "Hello there, Jay shri Krishna"
 
-app.run(host="0.0.0.0", port=80)
+app.run(host="0.0.0.0", port=5000)
